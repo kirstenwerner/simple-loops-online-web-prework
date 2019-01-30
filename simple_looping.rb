283972ee7f367aa47d62d8e85ce4171969195b1a
Kirsten Werner <kirstenmwerner@gmail.com>
2019-01-30 21:15:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,10 +21,11 @@ end
 
 def while_iterator(number_of_times)
   counter = 0 
-  while counter < number_of_times do 
-    
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  while counter < number_of_times
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+    counter += 1 
+  end
 end
 
 def until_iterator(number_of_times)
